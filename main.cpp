8050a3d35ce2388cd5dfb18b1c4aae304c32a1ce
0thw0foxa0 <97839499+0thw0foxa0@users.noreply.github.com>
2025-04-08 10:25:18 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,218 +1,253 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-// Функции для фрагмента №13 (сумма цифр числа)
-int F13_val(int n) { // Передача по значению
-    int s = 0, k;    // s - сумма, k - текущая цифра
-    for (; n != 0; n = n / 10) {
-        k = n % 10;  // Получаем последнюю цифру
-        s += k;      // Добавляем к сумме
-    }
-    return s;        // Возвращаем сумму цифр
-}
-
-void F13_ref(int n, int& s) { // Передача результата по ссылке
-    s = 0;                    // s - сумма (выходной параметр)
-    int k;                    // k - текущая цифра
-    for (; n != 0; n = n / 10) {
+// Программа №15: Разворачивает число
+// По значению
+int reverseNumber_val(int n) {
+    int s = 0, k;
+    while (n > 0) {
         k = n % 10;
-        s += k;
-    }
-}
-
-void F13_pointer(int n, int* s) { // Передача результата по указателю
-    *s = 0;                       // s - указатель на сумму
-    int k;                        // k - текущая цифра
-    for (; n != 0; n = n / 10) {
-        k = n % 10;
-        *s += k;
-    }
-}
-
-// Функции для фрагмента №15 (разворот числа)
-int F15_val(int n) { // Передача по значению
-    int s = 0, k;    // s - результат разворота, k - текущая цифра
-    for (; n != 0; n = n / 10) {
-        k = n % 10;
-        s = s * 10 + k; // Формируем число, добавляя цифры справа
+        s = s * 10 + k;
+        n /= 10;
     }
     return s;
 }
 
-void F15_ref(int n, int& s) { // Передача результата по ссылке
-    s = 0;                    // s - результат разворота
-    int k;                    // k - текущая цифра
-    for (; n != 0; n = n / 10) {
+// По ссылке
+void reverseNumber_ref(int n, int& s) {
+    s = 0;
+    int k;
+    while (n > 0) {
         k = n % 10;
         s = s * 10 + k;
+        n /= 10;
     }
 }
 
-void F15_pointer(int n, int* s) { // Передача результата по указателю
-    *s = 0;                       // s - указатель на результат
-    int k;                        // k - текущая цифра
-    for (; n != 0; n = n / 10) {
+// По указателю
+void reverseNumber_pointer(int n, int* s) {
+    *s = 0;
+    int k;
+    while (n > 0) {
         k = n % 10;
         *s = *s * 10 + k;
+        n /= 10;
     }
 }
 
-// Функции для фрагмента №18 (поиск палиндромов)
-void F18_val(int v, int A[], int& j) { // Передача массива по значению, j по ссылке
-    j = 0;                             // j - индекс для записи в массив
-    for (int a = 10; a < v; a++) {
-        int n = a, s = 0, k;
-        for (; n != 0; n = n / 10) {
-            k = n % 10;
-            s = s * 10 + k;
+// Программа №21: Сортировка массива методом пузырька
+// По значению (копия массива)
+void bubbleSort_val(int A[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (A[j] > A[j + 1]) {
+                int temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
         }
-        if (a == s) A[j++] = a; // Если число равно своему зеркалу, записываем
     }
 }
 
-void F18_ref(int v, int A[], int& j) { // Передача j по ссылке
-    j = 0;
-    for (int a = 10; a < v; a++) {
-        int n = a, s = 0, k;
-        for (; n != 0; n = n / 10) {
-            k = n % 10;
-            s = s * 10 + k;
+// По ссылке
+void bubbleSort_ref(int A[], int& n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (A[j] > A[j + 1]) {
+                int temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
         }
-        if (a == s) A[j++] = a;
     }
 }
 
-void F18_pointer(int v, int* A, int* j) { // Передача массива и j по указателю
-    *j = 0;
-    for (int a = 10; a < v; a++) {
-        int n = a, s = 0, k;
-        for (; n != 0; n = n / 10) {
-            k = n % 10;
-            s = s * 10 + k;
+// По указателю
+void bubbleSort_pointer(int* A, int* n) {
+    for (int i = 0; i < *n - 1; i++) {
+        for (int j = 0; j < *n - i - 1; j++) {
+            if (A[j] > A[j + 1]) {
+                int temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
         }
-        if (a == s) A[(*j)++] = a;
     }
 }
 
-// Функции для фрагмента №20 (удаление максимума из массива)
-void F20_val(int A[], int n) { // Передача по значению
-    int k = 0;                 // k - индекс максимума
-    for (int i = 1; i < n; i++)
-        if (A[i] > A[k]) k = i;
-    for (int j = k; j < n - 1; j++) A[j] = A[j + 1]; // Сдвиг элементов
+// Программа №39: Поиск минимального и максимального элементов
+// По значению
+void findMinMax_val(int A[], int n, int& min, int& max) {
+    min = A[0];
+    max = A[0];
+    for (int i = 1; i < n; i++) {
+        if (A[i] < min) min = A[i];
+        if (A[i] > max) max = A[i];
+    }
 }
 
-void F20_ref(int A[], int& n) { // Передача n по ссылке
-    int k = 0;
-    for (int i = 1; i < n; i++)
-        if (A[i] > A[k]) k = i;
-    for (int j = k; j < n - 1; j++) A[j] = A[j + 1];
-    n--; // Уменьшаем размер массива
+// По ссылке
+void findMinMax_ref(int A[], int n, int& min, int& max) {
+    min = A[0];
+    max = A[0];
+    for (int i = 1; i < n; i++) {
+        if (A[i] < min) min = A[i];
+        if (A[i] > max) max = A[i];
+    }
 }
 
-void F20_pointer(int* A, int* n) { // Передача по указателю
-    int k = 0;
-    for (int i = 1; i < *n; i++)
-        if (A[i] > A[k]) k = i;
-    for (int j = k; j < *n - 1; j++) A[j] = A[j + 1];
-    (*n)--; // Уменьшаем размер массива
+// По указателю
+void findMinMax_pointer(int* A, int n, int* min, int* max) {
+    *min = A[0];
+    *max = A[0];
+    for (int i = 1; i < n; i++) {
+        if (A[i] < *min) *min = A[i];
+        if (A[i] > *max) *max = A[i];
+    }
 }
 
-// Функции для фрагмента №23 (копирование простых чисел)
-void F23_val(int A[], int n, int B[], int& j) { // Передача по значению, j по ссылке
-    j = 0;                                      // j - индекс для массива B
+// Программа №48: Подсчёт гласных букв в строке
+// По значению
+int countVowels_val(string s) {
+    int count = 0;
+    for (char c : s) {
+        c = tolower(c);
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            count++;
+        }
+    }
+    return count;
+}
+
+// По ссылке
+void countVowels_ref(string s, int& count) {
+    count = 0;
+    for (char c : s) {
+        c = tolower(c);
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            count++;
+        }
+    }
+}
+
+// По указателю
+void countVowels_pointer(string s, int* count) {
+    *count = 0;
+    for (char c : s) {
+        c = tolower(c);
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            *count += 1;
+        }
+    }
+}
+
+// Программа №58: Сумма элементов главной диагонали
+// По значению
+int sumDiagonal_val(int A[][10], int n) {
+    int sum = 0;
     for (int i = 0; i < n; i++) {
-        int m;
-        for (m = 2; m < A[i]; m++)
-            if (A[i] % m == 0) break;
-        if (m == A[i]) B[j++] = A[i]; // Если число простое, копируем
+        sum += A[i][i];
     }
-    B[j] = 0; // Завершаем массив
+    return sum;
 }
 
-void F23_ref(int A[], int n, int B[], int& j) { // Передача j по ссылке
-    j = 0;
+// По ссылке
+void sumDiagonal_ref(int A[][10], int n, int& sum) {
+    sum = 0;
     for (int i = 0; i < n; i++) {
-        int m;
-        for (m = 2; m < A[i]; m++)
-            if (A[i] % m == 0) break;
-        if (m == A[i]) B[j++] = A[i];
+        sum += A[i][i];
     }
-    B[j] = 0;
 }
 
-void F23_pointer(int* A, int n, int* B, int* j) { // Передача по указателю
-    *j = 0;
+// По указателю
+void sumDiagonal_pointer(int* A, int n, int* sum) {
+    *sum = 0;
     for (int i = 0; i < n; i++) {
-        int m;
-        for (m = 2; m < A[i]; m++)
-            if (A[i] % m == 0) break;
-        if (m == A[i]) B[(*j)++] = A[i];
+        *sum += *(A + i * n + i); // Доступ к элементу A[i][i] через указатель
     }
-    B[*j] = 0;
+}
+
+// Функция для вывода массива
+void printArray(int A[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << " ";
+    }
 }
 
 int main() {
-    // Тест для F13
-    int s13_1, s13_2;
-    cout << "F13_val_1: " << F13_val(123) << endl;
-    F13_ref(456, s13_1);
-    cout << "F13_ref_1: " << s13_1 << endl;
-    F13_pointer(789, &s13_2);
-    cout << "F13_pointer_1: " << s13_2 << endl;
-    cout << "F13_val_2: " << F13_val(111) << endl;
-    F13_ref(222, s13_1);
-    cout << "F13_ref_2: " << s13_1 << endl;
-    F13_pointer(333, &s13_2);
-    cout << "F13_pointer_2: " << s13_2 << endl;
+    // Тесты для программы №15
+    int num1 = 123, num2 = 111, result;
+    cout << "F15_val_1: " << reverseNumber_val(num1) << endl;
+    reverseNumber_ref(num1, result);
+    cout << "F15_ref_1: " << result << endl;
+    reverseNumber_pointer(num1, &result);
+    cout << "F15_pointer_1: " << result << endl;
+    cout << "F15_val_2: " << reverseNumber_val(num2) << endl;
+    reverseNumber_ref(num2, result);
+    cout << "F15_ref_2: " << result << endl;
+    reverseNumber_pointer(num2, &result);
+    cout << "F15_pointer_2: " << result << endl;
 
-    // Тест для F15
-    int s15_1, s15_2;
-    cout << "F15_val_1: " << F15_val(123) << endl;
-    F15_ref(456, s15_1);
-    cout << "F15_ref_1: " << s15_1 << endl;
-    F15_pointer(789, &s15_2);
-    cout << "F15_pointer_1: " << s15_2 << endl;
-    cout << "F15_val_2: " << F15_val(111) << endl;
-    F15_ref(222, s15_1);
-    cout << "F15_ref_2: " << s15_1 << endl;
-    F15_pointer(333, &s15_2);
-    cout << "F15_pointer_2: " << s15_2 << endl;
+    // Тесты для программы №21
+    int A1[] = {5, 3, 1, 7, 9}, A2[] = {2, 0, 4, 6, 8};
+    int n1 = 5, n2 = 5;
+    bubbleSort_val(A1, n1);
+    cout << "F21_val_1: "; printArray(A1, n1); cout << endl;
+    bubbleSort_ref(A1, n1);
+    cout << "F21_ref_1: "; printArray(A1, n1); cout << endl;
+    bubbleSort_pointer(A1, &n1);
+    cout << "F21_pointer_1: "; printArray(A1, n1); cout << endl;
+    bubbleSort_val(A2, n2);
+    cout << "F21_val_2: "; printArray(A2, n2); cout << endl;
+    bubbleSort_ref(A2, n2);
+    cout << "F21_ref_2: "; printArray(A2, n2); cout << endl;
+    bubbleSort_pointer(A2, &n2);
+    cout << "F21_pointer_2: "; printArray(A2, n2); cout << endl;
 
-    // Тест для F18
-    int A18_1[10], A18_2[10], j18_1, j18_2;
-    F18_val(100, A18_1, j18_1);
-    cout << "F18_val_1: ";
-    for (int i = 0; i < j18_1; i++) cout << A18_1[i] << " ";
-    cout << endl;
-    F18_ref(50, A18_2, j18_2);
-    cout << "F18_ref_1: ";
-    for (int i = 0; i < j18_2; i++) cout << A18_2[i] << " ";
-    cout << endl;
+    // Тесты для программы №39
+    int min, max;
+    findMinMax_val(A1, n1, min, max);
+    cout << "F39_val_1: min=" << min << ", max=" << max << endl;
+    findMinMax_ref(A1, n1, min, max);
+    cout << "F39_ref_1: min=" << min << ", max=" << max << endl;
+    findMinMax_pointer(A1, n1, &min, &max);
+    cout << "F39_pointer_1: min=" << min << ", max=" << max << endl;
+    findMinMax_val(A2, n2, min, max);
+    cout << "F39_val_2: min=" << min << ", max=" << max << endl;
+    findMinMax_ref(A2, n2, min, max);
+    cout << "F39_ref_2: min=" << min << ", max=" << max << endl;
+    findMinMax_pointer(A2, n2, &min, &max);
+    cout << "F39_pointer_2: min=" << min << ", max=" << max << endl;
 
-    // Тест для F20
-    int A20_1[] = {3, 1, 4, 1, 5}, n20_1 = 5;
-    int A20_2[] = {2, 8, 1, 7}, n20_2 = 4;
-    F20_val(A20_1, n20_1);
-    cout << "F20_val_1: ";
-    for (int i = 0; i < n20_1 - 1; i++) cout << A20_1[i] << " ";
-    cout << endl;
-    F20_ref(A20_2, n20_2);
-    cout << "F20_ref_1: ";
-    for (int i = 0; i < n20_2; i++) cout << A20_2[i] << " ";
-    cout << endl;
+    // Тесты для программы №48
+    string s1 = "hello", s2 = "hi";
+    int count;
+    cout << "F48_val_1: " << countVowels_val(s1) << endl;
+    countVowels_ref(s1, count);
+    cout << "F48_ref_1: " << count << endl;
+    countVowels_pointer(s1, &count);
+    cout << "F48_pointer_1: " << count << endl;
+    cout << "F48_val_2: " << countVowels_val(s2) << endl;
+    countVowels_ref(s2, count);
+    cout << "F48_ref_2: " << count << endl;
+    countVowels_pointer(s2, &count);
+    cout << "F48_pointer_2: " << count << endl;
 
-    // Тест для F23
-    int A23_1[] = {4, 5, 6, 7}, n23_1 = 4, B23_1[10], j23_1;
-    int A23_2[] = {2, 3, 4, 5}, n23_2 = 4, B23_2[10], j23_2;
-    F23_val(A23_1, n23_1, B23_1, j23_1);
-    cout << "F23_val_1: ";
-    for (int i = 0; i < j23_1; i++) cout << B23_1[i] << " ";
-    cout << endl;
-    F23_ref(A23_2, n23_2, B23_2, j23_2);
-    cout << "F23_ref_1: ";
-    for (int i = 0; i < j23_2; i++) cout << B23_2[i] << " ";
-    cout << endl;
+    // Тесты для программы №58
+    int matrix1[10][10] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int matrix2[10][10] = {{1, 0, 0}, {0, 2, 0}, {0, 0, 3}};
+    int sum, n = 3;
+    cout << "F58_val_1: " << sumDiagonal_val(matrix1, n) << endl;
+    sumDiagonal_ref(matrix1, n, sum);
+    cout << "F58_ref_1: " << sum << endl;
+    sumDiagonal_pointer(&matrix1[0][0], n, &sum);
+    cout << "F58_pointer_1: " << sum << endl;
+    cout << "F58_val_2: " << sumDiagonal_val(matrix2, n) << endl;
+    sumDiagonal_ref(matrix2, n, sum);
+    cout << "F58_ref_2: " << sum << endl;
+    sumDiagonal_pointer(&matrix2[0][0], n, &sum);
+    cout << "F58_pointer_2: " << sum << endl;
 
     return 0;
 }
